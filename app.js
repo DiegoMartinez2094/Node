38f640f5a935add278eb6f0e7c4a77d29501c782
Diego Martinez <diegofernandomartinezcalderon@gmail.com>
2023-07-05 21:39:12 -0500

    feat: :rocket: prueba de union de HTML

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 import {createServer} from "http";
+import fs from "fs";
 const appHttp =createServer((req,res)=>{
-    res.end();
+  if(req.url == "/"){
+   fs.readFile("index.html", "utf8", (err, data)=>{
+    res.end(data);
+   });
+  }else if(req.url == "/home"){
+    fs.readFile("prueba.html", "utf8", (err, data)=>{
+      res.end(data);
+    })
+   }
 });
-
-
 const config = {
   hostname: "127.1.1.1",
-  port: 5005
+  port: 5000
 };
-
 appHttp.listen(config,()=>{
-    console.log('servidor activo');
-});
+    console.log(`http://${config.hostname}:${config.port}`);
+})
